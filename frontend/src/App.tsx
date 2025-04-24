b31a287625abfbeec2556b961db12b78fb5bdc01
Walerczik <xomathebest1@gmail.com>
2025-04-24 13:35:53 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-//import logo from './assets/logo.png';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="app-container">
-      //<img src={logo} alt="DurakOnTon Logo" className="logo" />
+      <img src={logo} alt="DurakOnTon Logo" className="logo" />
       <h1>DurakOnTon</h1>
       <p>Добро пожаловать! Подключайтесь к игре</p>
       <button onClick={() => alert('Скоро будет игра!')}>Играть</button>
