830054671ad6e8e062320e41a5df2504a0cbfa30
Walerczik <xomathebest1@gmail.com>
2025-04-25 09:20:34 +0200

    Update config.js

--- bot/config.js
+++ bot/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   BOT_TOKEN: "7612623843:AAGA8tNKRWI_gkywNBMT3VVgYJbvEdKC3kI",
-  FRONTEND_URL: "https://<your-vercel-app>.vercel.app"
+  FRONTEND_URL: "https://durak-on-ton.vercel.app"
 };
