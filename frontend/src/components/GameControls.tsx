cdea9a762b7e2ae568c93151e3735f0505b8ac34
Walerczik <xomathebest1@gmail.com>
2025-04-26 20:56:58 +0200

    Create GameControls.tsx

---
+++ frontend/src/components/GameControls.tsx
@@ -0,0 +1,10 @@
+const GameControls = () => {
+  return (
+    <div className="controls" style={{ marginTop: '20px' }}>
+      <button>Отбой</button>
+      <button>Беру</button>
+    </div>
+  );
+};
+
+export default GameControls;
