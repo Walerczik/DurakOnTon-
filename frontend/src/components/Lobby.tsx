eaa09139712229564aa9f88d5525a1cebd4ebeb7
Walerczik <xomathebest1@gmail.com>
2025-04-26 10:31:19 +0200

    Update Lobby.tsx

--- frontend/src/components/Lobby.tsx
+++ frontend/src/components/Lobby.tsx
@@ -1,41 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { Socket } from 'socket.io-client';
+import React from "react";
+import { Socket } from "socket.io-client";
 
 interface LobbyProps {
   socket: Socket;
 }
 
-interface Player {
-  id: string;
-  name: string;
-}
-
 const Lobby: React.FC<LobbyProps> = ({ socket }) => {
-  const [players, setPlayers] = useState<Player[]>([]);
-
-  useEffect(() => {
-    socket.on('playersUpdate', (updatedPlayers: Player[]) => {
-      setPlayers(updatedPlayers);
-    });
-
-    return () => {
-      socket.off('playersUpdate');
-    };
-  }, [socket]);
-
-  const handleStartGame = () => {
-    socket.emit('startGame');
+  const joinGame = () => {
+    socket.emit("joinGame");
   };
 
   return (
     <div>
       <h2>Lobby</h2>
-      <ul>
-        {players.map((player) => (
-          <li key={player.id}>{player.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleStartGame}>Start Game</button>
+      <button onClick={joinGame}>Join Game</button>
     </div>
   );
 };
