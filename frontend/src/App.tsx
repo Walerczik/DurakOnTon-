ee34e167d8c5bd853b93b4858dcfea272e127c3c
Walerczik <xomathebest1@gmail.com>
2025-04-26 11:22:17 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { io, Socket } from "socket.io-client";
-import GameTable, { Player } from "./components/GameTable";
-import Lobby from "./components/Lobby";
-
-const socket: Socket = io("http://localhost:3001");
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Lobby from './components/Lobby';
+import GameTable from './components/GameTable';
+import { GameProvider } from './context/GameContext';
 
 const App: React.FC = () => {
-  const [player, setPlayer] = useState<Player | null>(null);
-
-  useEffect(() => {
-    socket.on("playerData", (playerData: Player) => {
-      setPlayer(playerData);
-    });
-
-    return () => {
-      socket.off("playerData");
-    };
-  }, []);
-
   return (
-    <div className="App">
-      {player ? (
-        <GameTable socket={socket} player={player} />
-      ) : (
-        <Lobby socket={socket} />
-      )}
-    </div>
+    <GameProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Lobby />} />
+          <Route path="/game" element={<GameTable />} />
+        </Routes>
+      </Router>
+    </GameProvider>
   );
 };
 
