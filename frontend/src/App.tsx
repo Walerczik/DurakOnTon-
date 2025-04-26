a8b85139615fa0e6b34a6bc07e2be8adf6174821
Walerczik <xomathebest1@gmail.com>
2025-04-26 10:30:37 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import { io, Socket } from "socket.io-client";
 import Lobby from "./components/Lobby";
 import GameTable from "./components/GameTable";
-import { io, Socket } from "socket.io-client";
-import { Card } from "./components/GameTable";
+
+export interface Card {
+  suit: string;
+  rank: string;
+}
 
 export interface Player {
   id: string;
   hand: Card[];
 }
 
-const SOCKET_URL = process.env.REACT_APP_BACKEND_URL || "https://durakonton.onrender.com";
-const socket: Socket = io(SOCKET_URL);
+export interface GameState {
+  deck: Card[];
+  players: Player[];
+}
+
+const socket: Socket = io("https://durak-server-weld.vercel.app/");
 
 function App() {
   const [player, setPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
-    // Событие при инициализации — присвоение игрока
-    socket.on("init", (data: { player: Player }) => {
-      setPlayer(data.player);
+    socket.on("playerData", (playerData: Player) => {
+      setPlayer(playerData);
     });
-    // После чего сервер шлёт начальные данные и игрок считается «в лобби»
-    // Далее сервер будет рассылать «update» внутри GameTable
 
     return () => {
-      socket.off("init");
+      socket.off("playerData");
     };
   }, []);
 
