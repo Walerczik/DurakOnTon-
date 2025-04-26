6686588a940bc4145f35011f7836e7a500748fa7
Walerczik <xomathebest1@gmail.com>
2025-04-26 11:25:24 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lobby from './components/Lobby';
 import GameTable from './components/GameTable';
 import { GameProvider } from './context/GameContext';
