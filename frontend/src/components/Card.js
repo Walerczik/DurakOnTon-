c09b5e3a7cba48a3c6570316ce35fe216db6d676
Walerczik <xomathebest1@gmail.com>
2025-04-27 08:56:03 +0200

    Create Card.js

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cardBack from '../public/card_back.png';
+
+function Card({ hidden }) {
+  return (
+    <div className="card">
+      <img src={hidden ? cardBack : 'https://upload.wikimedia.org/wikipedia/commons/5/57/Playing_card_heart_6.svg'} alt="Card" />
+    </div>
+  );
+}
+
+export default Card;
